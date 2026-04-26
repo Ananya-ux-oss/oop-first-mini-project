a94cb19bc259cd441e5d5d3ac205dad41b5d4bb0
Ananya-ux-oss <Ananyalemma@gmail.com>
2026-04-26 06:59:38 -0700

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,1 +1,26 @@
+class Student extends Person {
+    private int id;
+    private static int totalStudents = 0;
 
+    public Student(String name, int id) {
+        super(name);
+        this.id = id;
+        totalStudents++;
+    }
+
+    // Overriding
+    @Override
+    public void showPerson() {
+        System.out.println("Student Name: " + name);
+    }
+
+    public void display() {
+        showPerson();
+        System.out.println("ID: " + id);
+        
+    }
+
+    public static int getTotalStudents() {
+        return totalStudents;
+    }
+}

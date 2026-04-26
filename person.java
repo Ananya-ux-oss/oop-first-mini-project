b18d470cf92ac1f0b77f249b9c8c0272b02f301b
Ananya-ux-oss <Ananyalemma@gmail.com>
2026-04-26 06:52:39 -0700

    Update person.java

--- person.java
+++ person.java
@@ -1,1 +1,16 @@
+class Person {
+    protected String name;
 
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public void showPerson() {
+        System.out.println("Name: " + name);
+    }
+
+    
+    public void showPerson(String msg) {
+        System.out.println(msg + " " + name);
+    }
+}
